97e1aa540e2ac5a218df1d323d7e86b51e446edd
Yuri Ritvin <yuri1992@gmail.com>
2015-07-22 08:52:46 +0300

    version deploy rest

--- login/views.py
+++ login/views.py
@@ -6,8 +6,18 @@ from facebook_sdk.facebook_helper import GraphAPIHelper
 from mongoengine import connect
 from models import Users, Stats
 from bson import ObjectId, Code
+from django.core.serializers.json import DjangoJSONEncoder
 import tasks
+import json
 connect('test', host='mongodb://localhost/test')
+
+
+class JsonMongodbEncoder(DjangoJSONEncoder):
+
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return super(JsonMongodbEncoder, self).defualt(o)
 
 
 class Login(ListView):
@@ -30,19 +40,29 @@ class Likes(ListView):
 
     def get(self, request, fb_id):
         res = {}
-        user_data = Users.objects.filter(fb_id=fb_id).first().values_list()
-        stats_data = Stats.objects.filter(fb_id=fb_id).first()
-        if user_data:
-            for x in user_data.photos[:10]:
-                print x.__dict__
-            res = {
-                'fb_id': user_data.fb_id,
-                'first_name': user_data.first_name,
-                'last_name': user_data.last_name,
-                'photos': [x.to_json() for x in user_data.photos[:10]],
-                #'posts': user_data.photos[:10],
-                #'videos': user_data.photos[:10],
-                'stats': {}
-            }
-        
-        return JsonResponse(res, safe=False)
+        res = Users.objects.\
+            filter(fb_id=fb_id).\
+            exclude('access_token').\
+            fields(slice__photos=10).\
+            fields(slice__posts=10).\
+            fields(slice__videos=10).\
+            first().\
+            to_mongo()
+
+        # print user_data
+        # print dir(user_data)
+        # stats_data = Stats.objects.filter(fb_id=fb_id).first()
+        # if user_data:
+        #     for x in user_data[0].photos[:10]:
+        #         print x.__dict__
+        #     res = {
+        #         'fb_id': user_data.fb_id,
+        #         'first_name': user_data.first_name,
+        #         'last_name': user_data.last_name,
+        #         'photos': [x.to_json() for x in user_data.photos[:10]],
+        # 'posts': user_data.photos[:10],
+        # 'videos': user_data.photos[:10],
+        #         'stats': {}
+        #     }
+
+        return JsonResponse(res, encoder=JsonMongodbEncoder, safe=False)
